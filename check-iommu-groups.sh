1a9de134c9969344df07f53bb8cef20af3d7faf5
Thomas Pham <tpham@ithings.ch>
2020-12-23 12:14:08 +0100

    my custom install on NixOS 20.09

---
+++ check-iommu-groups.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# change the 999 if needed
+shopt -s nullglob
+for d in /sys/kernel/iommu_groups/{0..999}/devices/*; do
+    n=${d#*/iommu_groups/*}; n=${n%%/*}
+    printf 'IOMMU Group %s ' "$n"
+    lspci -nns "${d##*/}"
+done;
